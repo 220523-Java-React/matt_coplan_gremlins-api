94ea1f43ef8822385f4104636dd9a3d9b782de41
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-01 11:42:03 -0400

    Binded controllers to Javalin on the Driver

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,17 @@
 package com.revature;
 
+import com.revature.controller.GremlinController;
+import io.javalin.Javalin;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        GremlinController gremlinController = new GremlinController();
+
+        Javalin app = Javalin.create().start(8080);
+
+        app.get("/",  ctx -> ctx.result("Welcome to Gremlin app!"));
+
+        app.get("/gremlins", gremlinController.getAllGremlins);
+        app.post("/gremlins", gremlinController.createNewGremlin);
     }
 }
