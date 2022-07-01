d3c7e44f4a57fa7d61215d3c0200ef16fd7ef638
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-01 11:07:43 -0400

    Created gremlin model

---
+++ src/main/java/com/revature/model/Gremlin.java
@@ -0,0 +1,80 @@
+package com.revature.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Gremlin implements Serializable {
+
+    private String name;
+    private int age;
+    private Color color;
+    private boolean isEvil;
+
+    public Gremlin() {}
+
+    public Gremlin(String name, int age, Color color, boolean isEvil) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.isEvil = isEvil;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Gremlin setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gremlin setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Gremlin setColor(Color color) {
+        this.color = color;
+        return this;
+    }
+
+    public boolean isEvil() {
+        return isEvil;
+    }
+
+    public Gremlin setEvil(boolean evil) {
+        isEvil = evil;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gremlin gremlin = (Gremlin) o;
+        return age == gremlin.age && isEvil == gremlin.isEvil && Objects.equals(name, gremlin.name) && color == gremlin.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, color, isEvil);
+    }
+
+    @Override
+    public String toString() {
+        return "Gremlin{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color=" + color +
+                ", isEvil=" + isEvil +
+                '}';
+    }
+}
