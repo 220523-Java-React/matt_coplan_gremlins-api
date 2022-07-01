ab80ce6911b78b7b4cc7dbfb1f848403dcfd6bf0
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-01 11:03:59 -0400

    Created database and connectionutility

---
+++ src/main/java/com/revature/utlis/ConnectionUtility.java
@@ -0,0 +1,29 @@
+package com.revature.utlis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtility {
+
+    //create private constructor
+    private ConnectionUtility(){}
+
+    //create instance of connection
+    public static Connection instance;
+
+    // Create String values for database ( should be in properties file!)
+    static final String url = "jdbc:postgresql://localhost:5432]/postgres?CurrentSchema=public";
+    static final String username = "postgres";
+    static final String password = "password";
+
+    // propagate exception
+    public static Connection getInstance() throws SQLException {
+
+        // check if instance is null or closed before opening
+        if (instance == null || instance.isClosed() ) {
+            instance = DriverManager.getConnection(url, username, password);
+        }
+        return instance;
+    }
+}
